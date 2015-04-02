a5a6ca2afcc4b84a72d1c6425b1892f2fccc2447
Wojciech Frącz <fraczwojciech@gmail.com>
2015-04-02 22:48:35 +0200

    Ex: save-your-work

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# git-exercises
+## Saving your work
+You are working hard on a regular issue while your boss comes in and wants you to fix a bug. State of your current
+working area is a total mess so you don't feel comfortable with making a commit now. However, you need to fix the found
+bug ASAP.
+
+Git lets you to save your work *on a side* and continue it later. Find appropriate Git tool and use it to handle
+the situation appropriately.
+
+Look for a bug to remove in `bug.txt`.
+
+After you commit the bugfix, get back to your work. Finish it by adding a new line to `bug.txt` with 
+
+    Finally, finished it!
+
+Then, commit your work after bugfix.
--- start.sh
+++ start.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
 
+echo "This file contains bug" > bug.txt
+echo "It has to be somewhere." >> bug.txt
+echo "I feel like I can smell it." >> bug.txt
+echo "THIS IS A BUG - remove the whole line to fix it." >> bug.txt
+echo "How this program could work with such bug?" >> bug.txt
+echo "This is a completely working program." > program.txt
+echo "This has been written in Brainfuck" >> program.txt
+echo "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>." >> program.txt
+git add bug.txt
+git add program.txt
+git commit -m "Excellent version with a bug"
+echo "This is the work I have done recently." >> bug.txt
+echo "I would be very sad if I loose it." >> bug.txt
+echo "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>." > program.txt
+echo ",----------[----------------------.,----------]" >> program.txt
+echo ",>,>++++++++[<------<------>>-]." >> program.txt
+echo "<<[>[>+>+<<-]>>[<<+>>-]<<<-]" >> program.txt
+echo ">>>++++++[<++++++++>-]<." >> program.txt
