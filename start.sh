e664d422e64f5140a4d46651c35cdbf7884ee38a
Wojciech Frącz <fraczwojciech@gmail.com>
2015-04-02 09:36:34 +0200

    Ex: remove-ignored

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 
+echo "This should be ignored" > ignored.txt
+git add ignored.txt
+git commit -m "Add ignored.txt"
+echo "ignored.txt" > .gitignore
+git add .gitignore
+git commit -m "Ignore ignored.txt"
