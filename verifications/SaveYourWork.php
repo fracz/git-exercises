03dc53d412eff301e402337cb97130b6fd52c19f
Wojciech Frącz <fraczwojciech@gmail.com>
2015-04-12 23:56:21 +0200

    Fix SaveYourWork verification

--- verifications/SaveYourWork.php
+++ verifications/SaveYourWork.php
@@ -16,7 +16,8 @@ class SaveYourWork extends AbstractVerification
         $this->ensure($buggy[3] == 'How this program could work with such bug?', "You haven't removed the bug in the first commit.");
         $this->ensureFilesCount($commits[0], 2);
         $buggy = $this->getFileContent($commits[0], 'bug.txt');
-        $this->ensure(count($buggy) == 7, 'You should have commited all your recent work in the last commit.');
+        $this->ensure(count($buggy) > 5, 'You should have committed all your recent work in the last commit.');
+        $this->ensure(count($buggy) == 7, 'You should have finished your recent work and committed it all in the last commit.');
         $this->ensure($buggy[6] == 'Finally, finished it!', "You haven't finished your work properly.");
     }
 }
