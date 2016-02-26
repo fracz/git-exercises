a97ca284dd422df348c5af6176066d5311da391e
Wojciech Frącz <fraczwojciech@gmail.com>
2016-02-26 16:49:13 +0100

    Update hook so it works with git http backend instead of Gerrit

--- hook.php
+++ hook.php
@@ -1,15 +1,9 @@
 #!/usr/bin/php
 <?php
 
-// https://gerrit.libreoffice.org/Documentation/config-hooks.html#_ref_update
-
-$project = $argv[2];
-$branch = $argv[4];
-$uploader = $argv[6];
-$oldRev = $argv[8];
-$newRev = $argv[10];
-
-$exerciseProjectName = 'git-exercises';
+$branch = $argv[1];
+$oldRev = $argv[2];
+$newRev = $argv[3];
 
 const GREEN = "[42m";
 const BLUE = "[44m";
@@ -17,65 +11,62 @@ const YELLOW = "[43m";
 const RED = "[41m";
 const PINK = "[45m";
 
-if ($project == $exerciseProjectName) {
+require __DIR__ . '/AbstractVerification.php';
 
-    require __DIR__ . '/AbstractVerification.php';
+$stars = str_repeat('*', 72);
 
-    $stars = str_repeat('*', 72);
+echo "(\n\n$stars\n";
 
-    echo "(\n\n$stars\n";
+/** @var AbstractVerification $verifier */
+$verifier = null;
 
-    /** @var AbstractVerification $verifier */
-    $verifier = null;
+$status = -1;
 
-    $status = -1;
-
-    if (strpos($branch, 'refs/heads/') === 0) {
-        $branch = substr($branch, strlen('refs/heads/'));
-    }
-
-    try {
-        $verifier = AbstractVerification::factory($branch, $oldRev, $newRev);
-    } catch (InvalidArgumentException $e) {
-        echo 'Status: ';
-        echo colorize('UNKNOWN EXERCISE', YELLOW);
-    }
-
-    if ($verifier) {
-        echo 'Exercise: ' . $verifier->getShortInfo() . PHP_EOL;
-        echo 'Status: ';
-        try {
-            $verifier->verify();
-            $status = 1;
-            echo colorize('PASSED', GREEN) . PHP_EOL;
-            $hints = $verifier->getHints();
-            if ($hints) {
-                $hints = explode('###', $hints);
-                echo PHP_EOL . colorize('The easiest solution:', PINK) . PHP_EOL . trim($hints[0]) . PHP_EOL;
-                echo PHP_EOL . 'Hints:' . PHP_EOL;
-                echo trim($hints[1]) . PHP_EOL . PHP_EOL;
-            }
-            $nextTask = getNextTask($branch);
-            if ($nextTask == 'master') {
-                echo colorize('Congratulations! You have done all exercises!', BLUE) . PHP_EOL;
-                echo 'Provided that you were doing them one by one :-)';
-            } else {
-                echo "Next task: $nextTask" . PHP_EOL;
-                echo "In order to start, execute: ";
-                echo colorize("git start $nextTask", BLUE);
-            }
-        } catch (VerificationFailure $e) {
-            $status = 0;
-            echo colorize('FAILED', RED) . PHP_EOL;
-            echo $e->getMessage();
-        }
-        @include __DIR__ . '/stats/stats.php';
-    }
-
-    echo "\n$stars\n\n)";
-
-    exit(1);
+if (strpos($branch, 'refs/heads/') === 0) {
+    $branch = substr($branch, strlen('refs/heads/'));
 }
+
+try {
+    $verifier = AbstractVerification::factory($branch, $oldRev, $newRev);
+} catch (InvalidArgumentException $e) {
+    echo 'Status: ';
+    echo colorize('UNKNOWN EXERCISE', YELLOW);
+}
+
+if ($verifier) {
+    echo 'Exercise: ' . $verifier->getShortInfo() . PHP_EOL;
+    echo 'Status: ';
+    try {
+        $verifier->verify();
+        $status = 1;
+        echo colorize('PASSED', GREEN) . PHP_EOL;
+        $hints = $verifier->getHints();
+        if ($hints) {
+            $hints = explode('###', $hints);
+            echo PHP_EOL . colorize('The easiest solution:', PINK) . PHP_EOL . trim($hints[0]) . PHP_EOL;
+            echo PHP_EOL . 'Hints:' . PHP_EOL;
+            echo trim($hints[1]) . PHP_EOL . PHP_EOL;
+        }
+        $nextTask = getNextTask($branch);
+        if ($nextTask == 'master') {
+            echo colorize('Congratulations! You have done all exercises!', BLUE) . PHP_EOL;
+            echo 'Provided that you were doing them one by one :-)';
+        } else {
+            echo "Next task: $nextTask" . PHP_EOL;
+            echo "In order to start, execute: ";
+            echo colorize("git start $nextTask", BLUE);
+        }
+    } catch (VerificationFailure $e) {
+        $status = 0;
+        echo colorize('FAILED', RED) . PHP_EOL;
+        echo $e->getMessage();
+    }
+    @include __DIR__ . '/stats/stats.php';
+}
+
+echo "\n$stars\n\n)";
+
+exit(1);
 
 function colorize($text, $color)
 {
