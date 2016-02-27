2dc0c83d33260d3ff3a1ac3af20fabd1b709cfd2
Wojciech Frącz <fraczwojciech@gmail.com>
2016-02-27 17:47:40 +0100

    New exercises alias

--- configure.sh
+++ configure.sh
@@ -7,7 +7,7 @@ git config alias.start "! f() { currentBranch=\$(git rev-parse --abbrev-ref HEAD
 git config alias.verify "! f() { currentBranch=\$(git rev-parse --abbrev-ref HEAD); exercise=\${1-\$currentBranch}; git show origin/\$exercise:start.sh >/dev/null 2>&1 && echo \"Verifying the \$exercise exercise. Hold on...\" && git push -f origin HEAD:\$exercise 2>&1 | sed -n '/\\*\\*\\*/,/\\*\\*\\*/p' | grep -v \"\\*\\*\" | sed 's/remote: //g' || echo \"Invalid exercise: \$exercise\"; }; f"
 
 # Add "git exercises" alias that shows list of available exercises
-git config alias.exercises "! echo \"Available exercises (alphabetically, use git start to switch): \" &&  git branch -r | grep origin | grep -v /HEAD | grep -v verifications | sed 's/origin\///g'"
+git config alias.exercises "! git push origin HEAD:exercises 2>&1 | sed -n '/\\*\\*\\*/,/\\*\\*\\*/p' | grep -v '\\*\\*' | sed 's/remote: //g'"
 
 # Make sure you have a "current" push strategy set; this will allow you to push only current exercise with simple git
 # push command instead of pushing all matching branches (default in Git < 2.0)
