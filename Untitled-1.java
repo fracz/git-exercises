447b8641383e0152e0a37bef2f51417eafb4ac38
EzekielResedenz <ighoresedenz@gmail.com>
2020-06-24 15:28:49 +0100

    Create Untitled-1.java

---
+++ Untitled-1.java
@@ -0,0 +1,7 @@
+git clone https://gitexercises.fracz.com/git/exercises.git
+cd exercises
+git config user.name "ezekielresedenz"
+git config user.email "ighoresedenz@gmail.com"
+./configure.sh
+git start
+git <i class="fas fa-infinity    "></i>
