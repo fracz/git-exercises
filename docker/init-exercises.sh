09aa41f5d9c992045a2fb52db90abf401c3a346d
Patrick Lafrance <plafrance@crosemont.qc.ca>
2023-03-23 17:54:34 -0400

    Various fixes to the building process

--- docker/init-exercises.sh
+++ docker/init-exercises.sh
@@ -24,19 +24,19 @@ git add .start.sh .verification.sh .teardown.sh && \
 git commit -m "Exercice base"
 
 # Create the branches
-cp /tmp/exercises/exercises/exercise-order.txt /var/www/website/backend/hook/
+cp /tmp/exercises/exercise-order.txt /var/www/website/backend/hook/
 shopt -s dotglob
-for exercise in $(cat /tmp/exercises/exercises/exercise-order.txt)
+for exercise in $(cat /tmp/exercises/exercise-order.txt)
 do
 	git checkout exercise-base
 	git checkout -b $exercise
-	cp -r /tmp/exercises/exercises/$exercise/files/* .
+	cp -r /tmp/exercises/$exercise/files/* .
 	git add .
 	git commit -m "Exercice files"
 
 	# Copy the hind, summary, solution and verification files into the backend
-	[ -f /tmp/exercises/exercises/$exercise/hint.md ] && cp /tmp/exercises/exercises/$exercise/hint.md /var/www/website/backend/hook/hints/$exercise-hint.md || true
-	[ -f /tmp/exercises/exercises/$exercise/summary.md ] && cp /tmp/exercises/exercises/$exercise/summary.md /var/www/website/backend/hook/hints/$exercise-summary.md || true
-	[ -f /tmp/exercises/exercises/$exercise/solution.txt ] && cp /tmp/exercises/exercises/$exercise/solution.txt /var/www/website/backend/hook/hints/$exercise-solution.txt || true
-	[ -f /tmp/exercises/exercises/$exercise/verification.php ] && cp /tmp/exercises/exercises/$exercise/verification.php /var/www/website/backend/hook/verifications/$( dashToCamelCase $exercise ).php || true
+	[ -f /tmp/exercises/$exercise/hint.md ] && cp /tmp/exercises/$exercise/hint.md /var/www/website/backend/hook/hints/$exercise-hint.md || true
+	[ -f /tmp/exercises/$exercise/summary.md ] && cp /tmp/exercises/$exercise/summary.md /var/www/website/backend/hook/hints/$exercise-summary.md || true
+	[ -f /tmp/exercises/$exercise/solution.txt ] && cp /tmp/exercises/$exercise/solution.txt /var/www/website/backend/hook/hints/$exercise-solution.txt || true
+	[ -f /tmp/exercises/$exercise/verification.php ] && cp /tmp/exercises/$exercise/verification.php /var/www/website/backend/hook/verifications/$( dashToCamelCase $exercise ).php || true
 done
